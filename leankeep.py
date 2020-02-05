d2aaa215bebf447d2405b5b3eec1a406adc15297
Patrick Dantas <patrickdsilva99@gmail.com>
2020-02-05 09:54:42 -0300

    selenium

---
+++ leankeep.py
@@ -0,0 +1,30 @@
+# -*- coding: utf8 -*-
+
+from bs4 import BeautifulSoup
+from mechanize import Browser
+import urllib.request
+import urllib.parse
+import re
+import sys
+import requests
+import mechanize
+import keys
+from selenium import webdriver
+
+def leankeep_a():
+
+	username_xpath = '//*[@id="ctl00_PageContent_UserName1"]'
+	password_xpath = '//*[@id="ctl00_PageContent_Password"]'
+	button_id = 'ctl00_PageContent_OKButton__Button'
+
+	driver = webdriver.Firefox()
+	driver.get("https://seguro.leankeep.com/leankeepX4/Security/SignIn.aspx")
+
+	driver.find_element_by_xpath(username_xpath).send_keys(keys.username)
+	driver.find_element_by_xpath(password_xpath).send_keys(keys.password)
+	page2 = driver.find_element_by_id(button_id).click()
+	
+	print(page2)
+
+
+leankeep_a()
